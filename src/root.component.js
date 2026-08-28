2db1ee2d012bc234c5d3e5f2492f8020d5ea1458
Felipe Pacheco <felipe.pacheco@bonin.eng.br>
2026-08-28 18:51:28 -0300

    feat: adiciona navegação entre microfrontends no navbar

--- src/root.component.js
+++ src/root.component.js
@@ -1,12 +1,27 @@
 export default function Root(props) {
+  const isProduction =
+    window.location.hostname === "bytebank-microfrontends.github.io";
+
+  const basePath = isProduction ? "/bytebank-orchestrator" : "";
+
   function onClick() {
     const event = new CustomEvent("onNavClick");
     document.dispatchEvent(event);
   }
+
   return (
     <section>
-      {props.name} is mounted!
-      <button onClick={onClick}>Broadcast do evento do navbar</button>
+      <nav>
+        <a href={`${basePath}/`}>Início</a>{" "}
+        <a href={`${basePath}/account`}>Conta</a>{" "}
+        <a href={`${basePath}/transaction`}>Transações</a>{" "}
+        <a href={`${basePath}/cards`}>Cartões</a>
+      </nav>
+
+      <div>
+        {props.name} is mounted!
+        <button onClick={onClick}>Broadcast do evento do navbar</button>
+      </div>
     </section>
   );
 }
