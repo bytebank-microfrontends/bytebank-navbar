cdff889b3af772a65b46f3cdb636c1feb4e1379b
Felipe Pacheco <felipe.pacheco@bonin.eng.br>
2026-08-29 10:14:26 -0300

    feat: adiciona símbolo oficial do ByteBank na sidebar

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,19 @@
 module.exports = {
   rootDir: "src",
+
   testEnvironment: "jsdom",
+
   transform: {
     "^.+\\.(j|t)sx?$": "babel-jest",
   },
+
   moduleNameMapper: {
     "\\.(css)$": "identity-obj-proxy",
+
+    "\\.(png|jpg|jpeg|gif|svg)$": "<rootDir>/../__mocks__/fileMock.js",
+
     "single-spa-react/parcel": "single-spa-react/lib/cjs/parcel.cjs",
   },
+
   setupFilesAfterEnv: ["@testing-library/jest-dom"],
 };
